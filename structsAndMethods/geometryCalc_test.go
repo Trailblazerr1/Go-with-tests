98f9621dbbb68b4412443e3386f842fa903bdf4d
Trailblazerr1 <kanurag078@gmail.com>
2024-12-18 11:34:54 +0530

    Add interface to decouple dependencies

--- structsAndMethods/geometryCalc_test.go
+++ structsAndMethods/geometryCalc_test.go
@@ -3,7 +3,8 @@ package geometry
 import "testing"
 
 func TestArea(t *testing.T) {
-	verifyResult := func(got, want float64, t *testing.T) {
+	verifyResult := func(shape Shape, want float64, t *testing.T) {
+		got := shape.Area()
 		if got != want {
 			t.Errorf("Got %.2f, want %.2f ", got, want)
 		}
@@ -11,17 +12,23 @@ func TestArea(t *testing.T) {
 
 	t.Run("Test area of rectangle", func(t *testing.T) {
 		rectangle := Rectangle{10, 20}
-		got := rectangle.Area()
 		want := 200.0
-		verifyResult(got, want, t)
+		verifyResult(rectangle, want, t)
 	})
 	//Go doesn't allows overloading of methods, so instead we have
 	//to create methods, that is call Area on different instances
 	//This is different from function
 	t.Run("Test area of circle", func(t *testing.T) {
 		circle := Circle{10}
-		got := circle.Area()
 		want := 314.15
-		verifyResult(got, want, t)
+		verifyResult(circle, want, t)
 	})
 }
+
+//Writing a table based tests for decoupling for shapes and tests
+
+// func TestArea(t *testing T) {
+// 	testParams := []struct {
+// 		Shape
+// 	}
+// }
--- structsAndMethods/geometryCalculation.go
+++ structsAndMethods/geometryCalculation.go
@@ -9,10 +9,26 @@ type Circle struct {
 	radius float64
 }
 
+type Triangle struct {
+	base   float64
+	height float64
+}
+
+// declare an interface shape which has a method Area,
+// which is similar to Rect, Circle and Triangle, since all of them have area
+// method. But none other have area method
+type Shape interface {
+	Area() float64
+}
+
 func (r Rectangle) Area() float64 {
 	return r.Height * r.Width //Here r is similar to this keyword
 }
 
 func (c Circle) Area() float64 {
 	return 314.15
+}
+
+func (t Triangle) Area() float64 {
+	return (t.base * t.height) * 0.5
 }
