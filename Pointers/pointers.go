2f97fa86e1fe9aec81f15f976a5883827b3850f7
Trailblazerr1 <kanurag078@gmail.com>
2024-12-23 09:25:29 +0530

    Add error handling to pointers

--- Pointers/pointers.go
+++ Pointers/pointers.go
@@ -1,11 +1,18 @@
 package crypto
 
+import (
+	"errors"
+)
+
 // give integer a specific name
 type ZCash int
 
 type Wallet struct {
 	balance ZCash
 }
+
+// var keyword allows us to have global variable
+var ErrInsufficientFunds = errors.New("Insufficient funds")
 
 // Just use * here to pass address, no need to dereference anywhere else
 func (w *Wallet) Deposit(amount ZCash) {
@@ -16,6 +23,11 @@ func (w *Wallet) Balance() ZCash {
 	return w.balance
 }
 
-func (w *Wallet) Withdraw(amount ZCash) {
+func (w *Wallet) Withdraw(amount ZCash) error {
+	bal := w.Balance()
+	if bal < amount {
+		return ErrInsufficientFunds
+	}
 	w.balance -= amount
+	return nil
 }
--- Pointers/pointers_test.go
+++ Pointers/pointers_test.go
@@ -10,6 +10,22 @@ func TestWallet(t *testing.T) {
 		}
 	}
 
+	verifyError := func(err error, t *testing.T, want error) {
+		if err == nil {
+			t.Fatal("Didn't return error while withdrawing insufficient funds")
+		}
+		//fatal breaks the flow and returns, so no null pointers
+		if err != want {
+			t.Errorf("Wanted %q got %q", want, err)
+		}
+	}
+
+	verifyNoError := func(err error, t *testing.T) {
+		if err != nil {
+			t.Fatal("Didn't want any error, but got one")
+		}
+	}
+
 	t.Run("Deposit money", func(t *testing.T) {
 		wallet := Wallet{ZCash(0)}
 		wallet.Deposit(ZCash(10))
@@ -19,8 +35,18 @@ func TestWallet(t *testing.T) {
 
 	t.Run("Withdraw money", func(t *testing.T) {
 		wallet := Wallet{ZCash(20)}
-		wallet.Withdraw(ZCash(10))
+		err := wallet.Withdraw(ZCash(10))
 		want := ZCash(10)
+		verifyNoError(err, t)
 		verifyCases(wallet, want, t)
+	})
+
+	t.Run("Withdraw money error", func(t *testing.T) {
+		initialAmt := ZCash(20)
+		wallet := Wallet{initialAmt}
+		err := wallet.Withdraw(ZCash(50))
+		verifyError(err, t, ErrInsufficientFunds)
+		verifyCases(wallet, initialAmt, t) //verify if balance is samee after failure
+		//of withdrawl
 	})
 }
