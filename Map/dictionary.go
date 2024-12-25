ebc473df0c27e1df3e3f121dab2f2c3fe89f67ca
Trailblazerr1 <kanurag078@gmail.com>
2024-12-25 14:03:32 +0530

    Dictionary CRUD operation

--- Map/dictionary.go
+++ Map/dictionary.go
@@ -7,6 +7,7 @@ import (
 type Dict map[string]string
 
 var errorKeyNotFound = errors.New("Key not found")
+var errorKeyAlreadyExists = errors.New("Key already exists")
 
 func (d Dict) Search(word string) (string, error) {
 	//map returns two value, first is the actual value and second is bool, whether val is found
@@ -16,4 +17,31 @@ func (d Dict) Search(word string) (string, error) {
 		return "", errorKeyNotFound
 	}
 	return value, nil
+}
+
+func (d Dict) Add(key, value string) (bool, error) {
+	_, err := d.Search(key)
+	if err == nil {
+		return false, errorKeyAlreadyExists
+	}
+	d[key] = value
+	return true, nil
+}
+
+func (d Dict) Update(key, value string) error {
+	_, err := d.Search(key)
+	if err != nil {
+		return errorKeyNotFound
+	}
+	d[key] = value
+	return nil
+}
+
+func (d Dict) Delete(key string) error {
+	_, err := d.Search(key)
+	if err != nil {
+		return errorKeyNotFound
+	}
+	delete(d, key)
+	return nil
 }
--- Map/dictionary_test.go
+++ Map/dictionary_test.go
@@ -1,6 +1,7 @@
 package dictionary
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -16,6 +17,20 @@ func TestSearch(t *testing.T) {
 		t.Helper()
 		if got != want {
 			t.Errorf("wanted %q, got %q", want, got)
+		}
+	}
+
+	verifyAdditionandUpdate := func(want, got bool, t *testing.T) {
+		t.Helper()
+		if got != want {
+			t.Errorf("wanted true, got false")
+		}
+	}
+
+	verifyDelete := func(want, err error, t *testing.T) {
+		t.Helper()
+		if err != want {
+			t.Errorf("Delete failed")
 		}
 	}
 
@@ -35,5 +50,40 @@ func TestSearch(t *testing.T) {
 			t.Fatal("Should've thrown an error")
 		}
 		verifyError(want, err, t)
+	})
+
+	t.Run("Add key value in dict", func(t *testing.T) {
+		dict := Dict{}
+		added, err := dict.Add("key", "value")
+		// want := true
+
+		if err != nil {
+			fmt.Print(err)
+			t.Fatal("Shouldn't have thrown an error")
+		}
+		verifyAdditionandUpdate(added, true, t)
+	})
+
+	t.Run("Update value", func(t *testing.T) {
+		dict := Dict{"Key": "Value"}
+		err := dict.Update("Key", "newValue")
+		if err != nil {
+			fmt.Print(err)
+			t.Fatal("Shouldn't have thrown an error")
+		}
+		// verifyAdditionandUpdate(added, true, t)
+	})
+
+	t.Run("Delete value", func(t *testing.T) {
+		dict := Dict{"Key": "Value"}
+		err := dict.Delete("Key")
+		if err != nil {
+			fmt.Print(err)
+			t.Fatal("Shouldn't have thrown an error")
+		}
+
+		_, err = dict.Search("Key")
+		want := errorKeyNotFound
+		verifyDelete(want, err, t)
 	})
 }
